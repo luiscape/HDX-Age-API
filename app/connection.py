48a1b40cbb70858b5440e53e346d73527bda3429
Reuben Cummings <reubano@gmail.com>
2015-09-10 15:20:30 +0300

    Minor refactoring

--- app/connection.py
+++ app/connection.py
@@ -12,20 +12,11 @@ from __future__ import (
 import redis
 from os import environ, getenv
 
-#
-# Check for Heroku environment variables
-#
-if environ.get('REDISTOGO_URL'):
-    redis_url = environ.get('REDISTOGO_URL')
 
-#
-# If not present, use Docker environment variable instead.
-#
-else:
-    redis_conn = getenv('REDIS_PORT_6379_TCP_ADDR', 'localhost')
-    redis_url = 'redis://' + redis_conn + ':6379'
+# Try Heroku environment variable.
+# If not present, use Docker environment variable.
+redis_host = getenv('REDIS_PORT_6379_TCP_ADDR', 'localhost')
+redis_url = environ.get('REDISTOGO_URL', 'redis://%s:6379' % redis_host)
 
-#
-# Connect to Redis.
-#
+# Create Redis connection
 conn = redis.from_url(redis_url)
